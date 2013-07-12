6bac301963cd6c6591e1224dabf38832330f7a97
bulfaitelo <bulfaitelo@gmail.com>
2013-07-12 15:54:00 -0300

    Create index.php

---
+++ PHP/envio_email/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="BULFAITELO" />
+
+	<title>testando envio de mail....</title>
+</head>
+
+<body>
+<form action="email.php" method="post">
+
+<label>Digite seu email:</label>
+<input  type="text" name="destinatario"/>
+<input type="submit" />
+
+
+</form>
+
+
+</body>
+</html>
